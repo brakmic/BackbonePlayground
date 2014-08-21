c3c84c4e7901d9d5331fc08c81ac4e89d89864b0
Harris Brakmic <brakmic@web.de>
2014-08-21 21:00:31 +0200

    added a simple event example (button click) in home view

--- views/home.js
+++ views/home.js
@@ -2,11 +2,17 @@ var templates = require('../templates/compiled');
 var domify = require('domify');
 
 module.exports = Backbone.View.extend({
+    events: {
+        'click input[type=button]': 'clickReact'
+    },
     initialize: function(){
         console.log('Home view loaded');
         this.render();
     },
     render: function(){
         this.$el.html(templates.body);
+    },
+    clickReact: function(e){
+        alert('Click');
     }
 });
