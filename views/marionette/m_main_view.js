8efeb7b328313b6c6a4f158584d30cc58d700dc3
brakmic <brakmic@gmail.com>
2014-08-29 10:54:58 +0200

    added a MarionetteJS example

--- views/marionette/m_main_view.js
+++ views/marionette/m_main_view.js
@@ -2,6 +2,5 @@ var _ = require('underscore');
 var templates = require('../../templates/compiled');
 
 module.exports = Backbone.Marionette.ItemView.extend({
-    name: 'Basic Marionette View',
     template: templates.marionette.m_main_view
 });
